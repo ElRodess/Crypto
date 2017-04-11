a1922c67c5daeaff20c6972dd38f835de669a449
Joseph <joseph.desquaires@ens.uvsq.fr>
2017-04-11 15:13:36 +0200

    Debut algo Fermat

---
+++ Fermat.c
@@ -0,0 +1,25 @@
+#include <gmp.h>
+#include <stdio.h>
+
+void TestFermat(mpz_t n, int k)
+{
+	int i = 0;
+	mpz_t a; 
+	gmp_randinit_default(a);
+	
+	while(i < k)
+	{
+		
+		i++;
+	}
+}
+
+void main()
+{
+mpz_t n,m ; 
+/*Premier */
+mpz_init_set_str(n,1106099);
+/* Pseudo Premier */
+mpz_init_set_str(m,41041);
+
+}
